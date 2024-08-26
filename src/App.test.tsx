c931302153c77be9258de6086d40e0fc2752007c
Hubert <hubert.zajac92@gmail.com>
2024-08-26 08:44:29 +0200

    commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('learn-link');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('url is correct', () => {
+  render(<App />);
+  const linkElement: HTMLAnchorElement = screen.getByTestId('learn-link');
+  expect(linkElement?.href).toContain('ultimateqa.com');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://ultimateqa.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          data-testid='learn-link'
         >
-          Learn React
+          Learn Testing and Dev
         </a>
       </header>
     </div>
